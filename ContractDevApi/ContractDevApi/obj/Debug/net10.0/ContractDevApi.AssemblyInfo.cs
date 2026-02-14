862e77fcc5dc60b9442ad3e18c1582b5b2257f26
Lorenzo <moares.lorenzo@gmail.com>
2026-02-14 14:05:55 +0000

    find user fixed

--- ContractDevApi/ContractDevApi/obj/Debug/net10.0/ContractDevApi.AssemblyInfo.cs
+++ ContractDevApi/ContractDevApi/obj/Debug/net10.0/ContractDevApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContractDevApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15a0c8a2286f79ef69615292804e376d05e17ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934d823d87297222f1580526bf393038f0e27f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContractDevApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContractDevApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
